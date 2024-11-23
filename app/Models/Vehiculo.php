2b0bfb09efe8fc1575c4e89fbcd99916ffeb5299
johanstevem <106841615+johanstevem@users.noreply.github.com>
2024-11-23 15:11:06 -0500

    Update Vehiculo.php

--- app/Models/Vehiculo.php
+++ app/Models/Vehiculo.php
@@ -9,10 +9,8 @@ class Vehiculo extends Model
 {
     use HasFactory;
 
-    // Si la tabla tiene un nombre diferente a la forma plural del modelo, defínelo aquí.
     protected $table = 'vehiculos';  
 
-    // Si deseas especificar las columnas que puedes asignar masivamente (Mass Assignment).
     protected $fillable = [
         'descripcion_de_vehiculos',  
         'categoria',                  
