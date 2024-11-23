5eb171ea3aed1768a14a062bc17541bb9eb0a49a
johanstevem <106841615+johanstevem@users.noreply.github.com>
2024-11-23 15:14:14 -0500

    Update 2024_11_17_140701_create_vehiculos_table.php

--- database/migrations/2024_11_17_140701_create_vehiculos_table.php
+++ database/migrations/2024_11_17_140701_create_vehiculos_table.php
@@ -6,30 +6,20 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
+   
     public function up()
     {
-        // Crear la tabla vehiculos sin tildes
         Schema::create('vehiculos', function (Blueprint $table) {
             $table->id();
-            $table->string('descripcion_de_vehiculos'); // Sin tilde en 'vehiculos'
-            $table->string('categoria'); // Si ya no se usará tilde en categoria, lo dejas así
+            $table->string('descripcion_de_vehiculos'); 
+            $table->string('categoria'); 
             $table->timestamps();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
+   
     public function down()
     {
-        // Eliminar la tabla vehiculos
         Schema::dropIfExists('vehiculos');
     }
 };
